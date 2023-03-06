6497af5e3d5608346d6c8355f7df56c73d81f20d
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-03-05 19:20:26 -0600

    Add files via upload

---
+++ arry_readers.c
@@ -0,0 +1,20 @@
+//let see if I can get this to read and write. And its the actaul input
+#include <stdio.h>
+
+int main(){
+  FILE *in = fopen("input.txt", "r");
+  char inp;
+  int n;
+  fscanf(in,"%d",&n);
+    while(n !=0){
+	printf("What files would you like to look into");
+}
+    
+
+
+}
+
+
+
+
+
