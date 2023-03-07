c2d1b613d05080e739daad2bf05da0da85a66877
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-03-06 23:15:38 -0600

    Add files via upload

--- arry_readers.c
+++ arry_readers.c
@@ -1,17 +1,26 @@
 //let see if I can get this to read and write. And its the actaul input
+//I should make a file for User interaction and one for checkin for files. hol up
 #include <stdio.h>
 
 int main(){
-  FILE *in = fopen("input.txt", "r");
-  char inp;
-  int n;
-  fscanf(in,"%d",&n);
-    while(n !=0){
-	printf("What files would you like to look into");
+  char *pw;
+  char *usrnam;
+
+
+  *in = fopen("username.txt","r");
+    while(usrnam = " "){
+    printf("Nice to meet you, I see your new here.",
+	    "Would you like to enter a username?");
+//need a ASCII or bit convert. Strings dont work 
+
+
 }
-    
 
-
+  FILE *in = fopen("password","r");//this should read 
+  fscanf(in,"%s",pw);
+  if(pw = ""){
+     printf("Please enter input");//this works so far, lets check for username.
+	     }
 }
 
 
