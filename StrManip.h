254ce73151833814448dc98628b9975fb618b7dd
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-03-27 22:01:37 -0500

    Add files via upload

--- StrManip.h
+++ StrManip.h
@@ -36,4 +36,26 @@ int isUpper(int b){
     return b;
 }
 
+int getline(char c[], int len){
+
+    int d = 0, e;
+
+    while(--len > 0 && (e = getchar())!=EOF && e!= '\n')
+	c[d++]  = e;
+    if(e=='\n')
+	c[d++] = e;
+    c[e] = '\0';
+    return d;
+
+}
+
+int isDigit(int f){
+
+    return f>='0' && f<='9'
+
+}
+
+
+//len
+
 #endif
