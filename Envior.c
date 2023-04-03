b42cbd292ef86fc20e60cc9fbf148ec25e0f8a9a
Jerimaine Hunt <100004627+JeriJamz@users.noreply.github.com>
2023-04-02 23:46:33 -0500

    Add files via upload

--- Envior.c
+++ Envior.c
@@ -27,8 +27,25 @@ static char *spc_preserve_environ[]={
 void spc_sanitize_(int preservec, char **preserveenv){//this is now an obj
 //preservec is an int, preserveenv take is a pointer and takes strings
 //It might convers prec input into its output
+    int i;
+    char **new_environ, *ptr, *value, *var;
+    size_t arr_size = 1, arr_ptr = 0, len, new_size = 0;//some of these are just vars
+       //      now spc_restrict is var also the array is set to i, and i++ means it increaments
+    for(i=0; (var = spc_restricted_environ[i]) !=0; i++){
 
+	new_size += strlen(var) + 1//remeber spc_restrict is a string so that means var is a str
+	arr_size++
 
+}
+//		I do need to clarify with the if state it only kick in when i doesnt equal zero or when the user make an input
+    for(i=0; (var = spc_restricted_environ[i]) !=0; i++){//this is the same as the last
+
+	if(!(value = getenv(var))) continue;
+	new_size += strlen(var) + strlen(var) + 2;//this will include the '=' we gone find out
+	arr_size++;
 
 }
 
+}
+
+new_size += (arr_size * sizeof(char*));new size will now be the size of the array.
